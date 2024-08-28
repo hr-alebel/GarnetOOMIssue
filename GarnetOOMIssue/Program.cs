db436afbf52196a77aa7bb0bbad4d1219c51c174
Alexander Belov <ale.bel@hr-zcs.com>
2024-08-28 10:13:04 +0200

    fixed logic

--- GarnetOOMIssue/Program.cs
+++ GarnetOOMIssue/Program.cs
@@ -6,7 +6,8 @@ namespace GarnetOOMIssue
 	{
 		static void Main(string[] args)
 		{
-			int expire_in_sec = 10;
+			int expire_in_sec_min = 15;
+			int expire_in_sec_max = 30;
 			List<StringBlock> blocks = new List<StringBlock>();
 			ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost");
 			IDatabase db = redis.GetDatabase();
@@ -26,8 +27,10 @@ namespace GarnetOOMIssue
 						int size = Random.Shared.Next(10, 90 * 1000 * 1000);
 						StringBlock block = new StringBlock();
 						block.data = Stringmockup.CreateLargeString(size, 'a');
+						int expire_in_sec = Random.Shared.Next(expire_in_sec_min, expire_in_sec_max);
 						block.expire = DateTime.Now.AddSeconds(expire_in_sec);
 						blocks.Add(block);
+						totalsize += block.length;
 						db.StringSet($"{Guid.NewGuid()}", block.data, new TimeSpan(0, 0, expire_in_sec));
 					}
 				}
